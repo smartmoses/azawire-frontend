ef2944375c08f969d836ffdcdacd9054aa479da8
Godswill Okoi <119078076+smartmoses@users.noreply.github.com>
2023-11-22 14:04:01 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx"
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  ) 
+};
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,8 @@
+const Hero = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
+};
+export default Hero;
